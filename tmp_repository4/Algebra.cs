7925bd0c68799955dc5c11a22f84a8f2bd34c37b
unknown <C:\The Bat!>
2023-05-16 19:49:22 +0700

    Log and NaturalLog

---
+++ tmp_repository4/Algebra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tmp_repository4 {
+    internal class Algebra {
+        public double naturalLog(double num) {
+            double log = Math.Log(num);
+            return log;
+        }
+        public double Log(double num, double newBase) {
+            double log = Math.Log(num,newBase);
+            return log;
+        }
+    }
+}
