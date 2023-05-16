6f2cfb3836f25c6f6301492f2f0d7fec1ec48e66
Morsus <shchepin1900@gmail.com>
2023-05-16 19:44:37 +0700

    Первая версия вычислений

---
+++ tmp_repository4/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tmp_repository4
+{
+    internal class Math
+    {
+        static double add(double numberA, double numberB)
+        {
+            return numberA + numberB;
+        }
+        static double sub(double numberA, double numberB)
+        {
+            return numberA - numberB;
+        }
+        static double mul(double numberA, double numberB)
+        {
+            return numberA * numberB;
+        }
+        static double div(double numberA, double numberB)
+        {
+            return numberA / numberB;
+        }
+    }
+}
